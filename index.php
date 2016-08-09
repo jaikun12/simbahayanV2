879f2a6bef51e36566a1952abf06869b528e03c1
boazcstrike <boazcstrike@yahoo.com.sg>
2016-08-09 20:22:37 +0800

    dbconnection

--- index.php
+++ index.php
@@ -45,6 +45,7 @@
   	}
   }
 ?>
+
 <html>
   <head>
     <meta charset="utf-8">
@@ -73,7 +74,7 @@
   <body class="hold-transition login-page">
     <div class="login-box">
       <div class="login-logo">
-        <a href="../../index2.html"><b>Simbahayan</b></a>
+        <b>Simbahayan</b>
       </div><!-- /.login-logo -->
       <div class="login-box-body">
         <p class="login-box-msg">Sign in to start your session</p>
